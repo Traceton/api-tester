dcd5574525ff436526ae7edfc23baec292364e9a
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-23 22:47:41 -0400

    small progress

--- src/App.js
+++ src/App.js
@@ -5,48 +5,97 @@ import './App.css';
 function App() {
 
   const [year, setYear] = useState("");
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [price, setPrice] = useState("");
+  const [sellerId, setSellerId] = useState("");
   const [picture, setPicture] = useState("");
 
   const createNew = async (e) => {
     e.preventDefault();
-    alert(year)
-    // const newItem = await new FormData();
-    // newItem.append("itemName", itemInfo.itemName);
-    // newItem.append("itemImage", itemImage);
-    // try {
-    //   if (itemImage && itemImage.size < 10000000) {
-    //     await Axios.post(
-    //       `http://localhost:3001//inventoryItems/createNew`,
-    //       newItem
-    //     ).then(async (response) => {
-    //       return response;
-    //     });
-    //   } else {
-    //     return alert("image size to large");
-    //   }
-    // } catch (error) {
-    //   return console.log("Invetory item create new item api error");
-    // }
+
+    const newItem = new FormData();
+    newItem.append("year", year);
+    newItem.append("make", make);
+    newItem.append("model", model);
+    newItem.append("price", price);
+    newItem.append("sellerId", sellerId);
+    newItem.append("vehicleImage", picture);
+
+    try {
+      if (picture && picture.size < 10000000) {
+        await Axios.post(
+          `http://localhost:3001//inventoryItems/createNew`,
+          newItem
+        ).then(async (response) => {
+          return response;
+        });
+      } else {
+        return alert("image size to large");
+      }
+    } catch (error) {
+      return console.log("Invetory item create new item api error");
+    }
   };
 
 
   return (
     <div className="App">
       <form onSubmit={createNew} method="post">
-        <label htmlFor="itemsInStock">itemsInStock</label>
+        <label htmlFor="year">itemsInStock</label>
         <input
           className=""
           type="text"
-          name="itemsInStock"
+          name="year"
           value={year}
           onChange={(event) => {
             setYear(event.target.value)
           }}
         ></input>
-        <label htmlFor="itemPicture">Item Pictures</label>
+        <label htmlFor="make">itemsInStock</label>
+        <input
+          className=""
+          type="text"
+          name="make"
+          value={make}
+          onChange={(event) => {
+            setMake(event.target.value)
+          }}
+        ></input>
+        <label htmlFor="model">itemsInStock</label>
+        <input
+          className=""
+          type="text"
+          name="model"
+          value={model}
+          onChange={(event) => {
+            setModel(event.target.value)
+          }}
+        ></input>
+        <label htmlFor="price">itemsInStock</label>
+        <input
+          className=""
+          type="text"
+          name="price"
+          value={price}
+          onChange={(event) => {
+            setPrice(event.target.value)
+          }}
+        ></input>
+        <label htmlFor="sellerId">itemsInStock</label>
+        <input
+          className=""
+          type="text"
+          name="sellerId"
+          value={sellerId}
+          onChange={(event) => {
+            setSellerId(event.target.value)
+          }}
+        ></input>
+        <label htmlFor="itemPicture">Item Picture</label>
         <input
           type="file"
-          // accept=".jpg"
+          accept=".jpg"
           onChange={(Event) => {
             const file = Event.target.files[0];
             setPicture(file);
