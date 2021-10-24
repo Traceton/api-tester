1758c20b4d4e443457f86219459807381451595b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-23 21:32:14 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
+import Axios from "axios";
 import './App.css';
 
 function App() {
+
+  const createNew = async (
+    itemInfo,
+    itemImage
+  ) => {
+    const newItem = await new FormData();
+    newItem.append("itemName", itemInfo.itemName);
+    newItem.append("itemImage", itemImage);
+    try {
+      if (itemImage && itemImage.size < 10000000) {
+        await Axios.post(
+          `http://localhost:3001//inventoryItems/createNew`,
+          newItem
+        ).then(async (response) => {
+          return response;
+        });
+      } else {
+        return alert("image size to large");
+      }
+    } catch (error) {
+      return console.log("Invetory item create new item api error");
+    }
+  };
+
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <button onSubmit={createNew}>
+        hello
+      </button>
     </div>
   );
 }
