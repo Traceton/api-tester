61bf63a87e166f468c4286a909b05db913ffc92a
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-10-23 21:53:11 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
+import React, { useState } from "react";
 import Axios from "axios";
 import './App.css';
 
 function App() {
 
-  const createNew = async (
-    itemInfo,
-    itemImage
-  ) => {
-    const newItem = await new FormData();
-    newItem.append("itemName", itemInfo.itemName);
-    newItem.append("itemImage", itemImage);
-    try {
-      if (itemImage && itemImage.size < 10000000) {
-        await Axios.post(
-          `http://localhost:3001//inventoryItems/createNew`,
-          newItem
-        ).then(async (response) => {
-          return response;
-        });
-      } else {
-        return alert("image size to large");
-      }
-    } catch (error) {
-      return console.log("Invetory item create new item api error");
-    }
+  const [year, setYear] = useState("");
+
+  const createNew = async (e) => {
+    e.preventDefault();
+    alert(year)
+    // const newItem = await new FormData();
+    // newItem.append("itemName", itemInfo.itemName);
+    // newItem.append("itemImage", itemImage);
+    // try {
+    //   if (itemImage && itemImage.size < 10000000) {
+    //     await Axios.post(
+    //       `http://localhost:3001//inventoryItems/createNew`,
+    //       newItem
+    //     ).then(async (response) => {
+    //       return response;
+    //     });
+    //   } else {
+    //     return alert("image size to large");
+    //   }
+    // } catch (error) {
+    //   return console.log("Invetory item create new item api error");
+    // }
   };
 
 
   return (
     <div className="App">
-      <button onSubmit={createNew}>
-        hello
-      </button>
+      <form onSubmit={createNew} method="post">
+        <label htmlFor="itemsInStock">itemsInStock</label>
+        <input
+          className="bg-gray-350 rounded text-center p-1"
+          type="text"
+          name="itemsInStock"
+          value={year}
+          onChange={(event) => {
+            setYear(event.target.value)
+          }}
+        ></input>
+        <button type="submit">
+          submit
+        </button>
+      </form>
+
     </div>
   );
 }
